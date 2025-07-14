dd4774b09d0adf2c46e359fc0e4bdd5d71f2b654
thevishwaak <vishwaqx@gmail.com>
2025-07-14 09:02:39 +0530

    Add files via upload

---
+++ Industrial Automation/Temperature Node/main.d
@@ -0,0 +1,10 @@
+main.o: main.c
+main.o: C:\KeilARM\ARM\INC\Philips\lpc21xx.h
+main.o: delay.h
+main.o: types.h
+main.o: lcd.h
+main.o: can.h
+main.o: spi_defines.h
+main.o: spi.h
+main.o: mcp3208.h
+main.o: motor.h
